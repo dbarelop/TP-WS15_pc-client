c4c5a96ce92554a1fbeb49197aca98a6492a3014
Andreas Maier <andmaier@htwg-konstanz.de>
2016-01-18 12:32:19 +0100

    added commands as listed in the readme

--- pc-client/Commands.cs
+++ pc-client/Commands.cs
@@ -19,14 +19,15 @@ namespace pc_client
         public const byte ADT =     0x30; // 0011_0000
 
         // Commands --> 0000_XXXX
-        public const byte READ =    0x00; // 0000_0000
-        public const byte WRITE =   0x01; // 0000_0001
-        public const byte ERASE =   0x02; // 0000_0010
-
-        public const byte CH1 =     0x03; // 0000_0011
-        public const byte CH2 =     0x04; // 0000_0100
-        public const byte RNG1 =    0x05; // 0000_0101  +-2.50V
-        public const byte RNG2 =    0x06; // 0000_0110  +-0.16V      
+        public const byte READ =            0x00; // 0000_0000
+        public const byte WRITE =           0x01; // 0000_0001
+        public const byte ERASE =           0x02; // 0000_0010
+        public const byte CH1 =             0x03; // 0000_0011
+        public const byte CH2 =             0x04; // 0000_0100
+        public const byte RNG1 =            0x05; // 0000_0101  +-2.50V
+        public const byte RNG2 =            0x06; // 0000_0110  +-0.16V      
+        public const byte EEPROM_READ =     0x07; // 0000_0111  read 16bit command   
+        public const byte EEPROM_WRITE =    0x08; // 0000_1000  write 16bit command     
 
         #endregion
 
